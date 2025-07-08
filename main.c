a51c87a6b56a95419a059ad2c91b1f231116b155
Gne <gneiman@narod.ru>
2025-07-09 01:32:38 +0700

    add

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
-int main(int, char**){
+typedef struct StatData {
+long id;
+int
+count;
+float cost;
+unsigned int primary:1;
+unsigned int mode:3;
+} StatData;
+
+
+int StoreDump();
+int LoadDump();
+int JoinDump();
+int SortDump();
+
+
+int main(int argc, char** argv){
     printf("Hello, from analyze!\n");
 }
